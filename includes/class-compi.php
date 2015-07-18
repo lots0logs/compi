75ce3c218af377629388158fcc775f41dab7db96
Catalin Vasile <contact@catalin.me>
2015-07-18 11:50:13 +0300

    More simplification.
    - Removed useless getters.
    - Added comments.
    - Modified access.

--- includes/class-compi.php
+++ includes/class-compi.php
@@ -34,28 +34,25 @@ class Compi {
 	 * The unique identifier of this plugin.
 	 *
 	 * @since    1.0.0
-	 * @access   protected
 	 * @var      string $plugin_name The string used to uniquely identify this plugin.
 	 */
-	protected $plugin_name;
+	public $plugin_name;
 
 	/**
 	 * The current version of the plugin.
 	 *
 	 * @since    1.0.0
-	 * @access   protected
 	 * @var      string $version The current version of the plugin.
 	 */
-	protected $version;
+	public $version;
 
 	/**
 	 * The plugin's dashboard.
 	 *
 	 * @since    1.0.0
-	 * @access   protected
 	 * @var      string $dashboard The admin dashboard.
 	 */
-	protected $dashboard;
+	public $dashboard;
 
 	/**
 	 * Define the core functionality of the plugin.
@@ -68,7 +65,10 @@ class Compi {
 	 */
 	public function __construct( $version ) {
 
+		// Set plugin's name (and slug).
 		$this->plugin_name = 'compi';
+
+		// Set plugin's version.
 		$this->version     = $version;
 
 		// Register activation hook.
@@ -77,9 +77,16 @@ class Compi {
 		// Register deactivation hook.
 		register_deactivation_hook( __FILE__, array( 'Compi', 'deactivation_hook' ) );
 
+		// Load all plugin dependencies.
 		$this->load_dependencies();
+
+		// Set localization.
 		$this->set_locale();
+
+		// Define dashboard hooks.
 		$this->define_dashboard_hooks();
+
+		// Define public hooks.
 		$this->define_public_hooks();
 
 	}
@@ -133,33 +140,10 @@ class Compi {
 	private function set_locale() {
 
 		$plugin_i18n = new Compi_i18n();
-		$plugin_i18n->set_domain( $this->get_plugin_name() );
+		$plugin_i18n->set_domain( $this->plugin_name );
 
 		add_action( 'plugins_loaded', array( $plugin_i18n, 'load_plugin_textdomain' ) );
 
-	}
-
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function get_plugin_name() {
-
-		return $this->plugin_name;
-	}
-
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function get_version() {
-
-		return $this->version;
 	}
 
 	/**
@@ -171,7 +155,7 @@ class Compi {
 	 */
 	private function define_dashboard_hooks() {
 
-		$this->dashboard = new Compi_Admin( $this->get_plugin_name(), $this->get_version() );
+		$this->dashboard = new Compi_Admin( $this->plugin_name, $this->version );
 
 		add_action( 'admin_menu', array( $this->dashboard, 'setup_dashboard' ), 90 );
 
@@ -186,7 +170,7 @@ class Compi {
 	 */
 	private function define_public_hooks() {
 
-		$plugin_public = new Compi_Public( $this->get_plugin_name(), $this->get_version() );
+		$plugin_public = new Compi_Public( $this->plugin_name, $this->version );
 
 		add_action( 'wp_enqueue_scripts', array( $plugin_public, 'enqueue_styles' ) );
 		add_action( 'wp_enqueue_scripts', array( $plugin_public, 'enqueue_scripts' ) );
