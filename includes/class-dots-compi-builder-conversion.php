32257f425e2309f5a213e69e7776bb0f6414db0a
Dustin Falgout <dustin@falgout.us>
2015-10-21 02:37:36 -0500

    ***wonders when he'll get some help with this***

--- includes/class-dots-compi-builder-conversion.php
+++ includes/class-dots-compi-builder-conversion.php
@@ -49,7 +49,7 @@ class Dots_Compi_Conversion_Util {
 
 		self::$_this = $this;
 
-		$this->map              = self::get_eb_to_divi_builder_mapping();
+		$this->map              = Dots_Compi_EBuilder_Mapping::$mapping;
 		$this->eb_settings      = get_option( 'et_lb_main_settings' );
 		$this->eb_post_types    = isset( $this->eb_settings['post_types'] )
 			? (array) $this->eb_settings['post_types']
@@ -359,176 +359,16 @@ class Dots_Compi_Conversion_Util {
 				$this->rows[]    = $new_row;
 				$new_row         = new Dots_Compi_EBuilder_Row( $index );
 				$shortcode_index = 0;
+			} else {
+				$shortcode_index += 1;
 			}
 			$new_row->add_child( $shortcode );
 			$index += 1;
-			$shortcode_index += 1;
+
 		}
 		$this->rows[] = $new_row;
 	}
 
-	public static function get_eb_to_divi_builder_mapping() {
-
-		return array(
-			'et_lb_logo'          => array(
-				'new_slug' => 'et_pb_image',
-				'attrs'    => array(
-					'align'   => 'align',
-					'content' => 'url',
-				),
-			),
-			'et_lb_paper'         => array(
-				'new_slug' => 'et_pb_text',
-				'attrs'    => array(
-					'text'      => 'content_new',
-					'css_class' => 'module_class',
-				),
-			),
-			'et_lb_video'         => array(
-				'new_slug' => 'et_pb_text',
-				'attrs'    => array(
-					'video_url' => 'content_new',
-					'class'     => 'module_class',
-				),
-			),
-			'et_lb_testimonial'   => array(
-				'new_slug' => 'et_pb_testimonial',
-				'attrs'    => array(
-					'image_url'       => 'portrait_url',
-					'author_name'     => 'author',
-					'author_position' => 'job_title',
-					'author_site'     => 'url',
-					'content'         => 'content_new',
-				),
-			),
-			'et_lb_slogan'        => array(
-				'new_slug' => 'et_pb_text',
-				'attrs'    => array(
-					'class'   => 'module_class',
-					'content' => 'content_new',
-				),
-			),
-			'et_lb_slider'        => array(
-				'new_slug'  => 'et_pb_gallery',
-				'attrs'     => array(
-					'animation_duration' => 'auto_speed',
-					'auto_animation'     => 'auto',
-					'class'              => 'module_class',
-					'images'             => 'src',
-				),
-				'attrs_new' => array(
-					'fullwidth' => 'no',
-				),
-			),
-			'et_lb_button'        => array(
-				'new_slug' => 'et_lb_button',
-				'attrs'    => array(
-					'class' => 'module_class',
-				),
-			),
-			'et_lb_bar'           => array(
-				'new_slug' => 'et_pb_divider',
-				'attrs'    => array(
-					'class' => 'module_class',
-				),
-			),
-			'et_lb_list'          => array(
-				'new_slug' => 'et_pb_text',
-				'attrs'    => array(
-					'class'   => 'module_class',
-					'content' => 'content_new',
-				),
-			),
-			'et_lb_toggle'        => array(
-				'new_slug' => 'et_pb_toggle',
-				'attrs'    => array(
-					'heading' => 'title',
-					'state'   => 'open',
-					'class'   => 'module_class',
-					'content' => 'content_new',
-				),
-			),
-			'et_lb_simple_slider' => array(
-				'new_slug' => 'et_pb_slider',
-				'attrs'    => array(
-					'class' => 'module_class',
-				),
-			),
-			'et_lb_simple_slide'  => array(
-				'new_slug' => 'et_pb_slide',
-				'attrs'    => array(
-					'class' => 'module_class',
-				),
-			),
-			'et_lb_tabs'          => array(
-				'new_slug' => 'et_pb_tabs',
-			),
-			'et_lb_tab'           => array(
-				'new_slug' => 'et_pb_tab',
-				'attrs'    => array(),
-			),
-			'et_lb_pricing_table' => array(
-				'new_slug' => 'et_pb_pricing',
-				'attrs'    => array(),
-			),
-			'et_lb_box'           => array(
-				'new_slug' => 'et_pb_cta',
-				'attrs'    => array(),
-			),
-			'et_lb_text_block'    => array(
-				'new_slug' => 'et_pb_text',
-				'attrs'    => array(),
-			),
-			'et_lb_widget_area'   => array(
-				'new_slug' => 'et_pb_slider',
-				'attrs'    => array(),
-			),
-			'et_lb_image'         => array(
-				'new_slug' => 'et_pb_image',
-				'attrs'    => array(),
-			),
-			'et_lb_1_2'           => array(
-				'new_slug'  => 'et_pb_column',
-				'attrs'     => array(),
-				'attrs_new' => array(
-					'type' => '1_2',
-				),
-			),
-			'et_lb_1_3'           => array(
-				'new_slug'  => 'et_pb_column',
-				'attrs'     => array(),
-				'attrs_new' => array(
-					'type' => '1_3',
-				),
-			),
-			'et_lb_1_4'           => array(
-				'new_slug'  => 'et_pb_column',
-				'attrs'     => array(),
-				'attrs_new' => array(
-					'type' => '1_4',
-				),
-			),
-			'et_lb_2_3'           => array(
-				'new_slug'  => 'et_pb_column',
-				'attrs'     => array(),
-				'attrs_new' => array(
-					'type' => '2_3',
-				),
-			),
-			'et_lb_3_4'           => array(
-				'new_slug'  => 'et_pb_column',
-				'attrs'     => array(),
-				'attrs_new' => array(
-					'type' => '3_4',
-				),
-			),
-			'et_lb_resizable'     => array(
-				'new_slug'  => 'et_pb_column',
-				'attrs'     => array(),
-				'attrs_new' => array(),
-			),
-		);
-	}
 }
 
 
@@ -537,11 +377,17 @@ class Dots_Compi_EBuilder_Element {
 	protected $_children = array();
 	protected $_attrs = array();
 	public $as_string = '';
-	public $contents = '';
+	public $contents;
 	public $column_wrapper = false;
 	public $is_row = false;
-	public $slug = '';
-	public $new_slug = '';
+	public $slug;
+	public $new_slug;
+	public $parent = '';
+	public $is_column = false;
+	public $is_resizable = false;
+	public $old_attrs;
+	public $index;
+
 
 	public function get_len() {
 
@@ -550,14 +396,14 @@ class Dots_Compi_EBuilder_Element {
 
 	public function add_child( &$child ) {
 
-		$this->_children[] = $child;
+		$this->_children[] = &$child;
 	}
 
 	public function get_child( $index ) {
 
 		$child = '';
 		if ( isset( $this->_children[ $index ] ) ) {
-			$child = $this->_children[ $index ];
+			$child = &$this->_children[ $index ];
 		}
 
 		return $child;
@@ -566,6 +412,10 @@ class Dots_Compi_EBuilder_Element {
 	public function get_as_string() {
 
 		if ( '' === $this->as_string ) {
+
+			if ( '' !== $this->parent ) {
+				$this->maybe_add_column_wrapper();
+			}
 
 			$self_string = '[' . $this->new_slug;
 
@@ -576,7 +426,7 @@ class Dots_Compi_EBuilder_Element {
 			}
 			$self_string .= ']';
 			if ( ! empty( $this->_children ) ) {
-				foreach ( $this->_children as $child_index => $child ) {
+				foreach ( $this->_children as $child_index => &$child ) {
 					$self_string .= $child->get_as_string();
 				}
 			} else {
@@ -594,6 +444,58 @@ class Dots_Compi_EBuilder_Element {
 		}
 
 		return $self_string;
+	}
+
+	public function in_range( $val, $min, $max ) {
+
+		return ( $val >= $min && $val <= $max );
+	}
+
+	public function maybe_add_column_wrapper() {
+
+		if ( true === $this->parent->is_row && false === $this->is_column ) {
+			$siblings = $this->parent->get_len();
+			if ( 2 === $siblings && array_key_exists( $this->old_attrs, 'width' ) ) {
+				$width         = (int) $this->old_attrs['width'];
+				$sibling_index = ( 0 === $this->index ) ? 1 : 0;
+				$sibling       = $this->parent->get_child( $sibling_index );
+				$column_string = $sibling->column_wrapper;
+				if ( '1_4' === $column_string || $this->in_range( $width, 66, 100 ) ) {
+					$this->column_wrapper = '3_4';
+				} elseif ( '1_3' === $column_string || $this->in_range( $width, 56, 65 ) ) {
+					$this->column_wrapper = '2_3';
+				} elseif ( '1_2' === $column_string || $this->in_range( $width, 49, 55 ) ) {
+					$this->column_wrapper = '1_2';
+				} elseif ( '2_3' === $column_string || $this->in_range( $width, 26, 48 ) ) {
+					$this->column_wrapper = '1_3';
+				} elseif ( '3_4' === $column_string || $this->in_range( $width, 0, 25 ) ) {
+					$this->column_wrapper = '1_4';
+				}
+
+			} else {
+				switch ( $siblings ) {
+					case 2:
+						$this->column_wrapper = '1_2';
+						break;
+					case 1:
+						$this->column_wrapper = '4_4';
+						break;
+					case 3:
+						$this->column_wrapper = '1_3';
+						break;
+					case 4:
+						$this->column_wrapper = '1_4';
+						break;
+					default:
+						Dots_Compi_Conversion_Util::write_log( array( 'COLUMN WRAPPER ERROR' => 'TRUE', '$this->column_wrapper' => $this->column_wrapper ) );
+				}
+			}
+
+			if ( true === $this->is_resizable ) {
+				$this->_attrs['type'] = $this->column_wrapper;
+				$this->column_wrapper = false;
+			}
+		}
 	}
 }
 
@@ -623,23 +525,23 @@ class Dots_Compi_EBuilder_Row extends Dots_Compi_EBuilder_Element {
 }
 
 class Dots_Compi_EBuilder_Shortcode extends Dots_Compi_EBuilder_Element {
-	public function __construct( &$parent, $index, $as_string, $slug, $attrs_string, $contents ) {
+	public function __construct( &$parent, $index, $old_as_string, $slug, $attrs_string, $contents ) {
 
 		$this->slug           = $slug;
 		$this->index          = $index;
 		$this->parent         = &$parent;
 		$this->column_wrapper = false;
-		$this->map            = Dots_Compi_Conversion_Util::get_eb_to_divi_builder_mapping();
+		$this->map            = Dots_Compi_EBuilder_Mapping::$mapping;
 		$this->new_slug       = isset( $this->map[ $slug ]['new_slug'] ) ? $this->map[ $slug ]['new_slug'] : '';
-		$this->is_column      = preg_match( '/(et_lb_\d_\d)/', $this->slug );
-		$this->is_resizable   = preg_match( '/(resizable)/', $this->slug );
+		$this->is_column      = preg_match( '/et_lb_\d_\d/', $this->slug );
+		$this->is_resizable   = preg_match( '/resizable/', $this->slug );
 		$this->contents       = $contents;
 		$this->old_attrs      = $this->parse_attributes( $attrs_string );
 		$this->is_first       = array_key_exists( 'first_class', $this->old_attrs );
 
 
 		$this->process_children();
-		$this->maybe_add_column_wrapper();
+		//$this->maybe_add_column_wrapper();
 	}
 
 	/**
@@ -685,13 +587,13 @@ class Dots_Compi_EBuilder_Shortcode extends Dots_Compi_EBuilder_Element {
 		Dots_Compi_Conversion_Util::write_log( $nested_tags );
 		if ( is_array( $nested_tags ) && ! empty( $nested_tags ) ) {
 			foreach ( $nested_tags[2] as $nested_tag => $slug ) {
-				$as_string    = isset( $nested_tags[0][ $index ] ) ? $nested_tags[0][ $index ] : '';
-				$attrs_string = isset( $nested_tags[3][ $index ] ) ? $nested_tags[3][ $index ] : '';
-				$contents     = isset( $nested_tags[5][ $index ] ) ? $nested_tags[5][ $index ] : '';
+				$old_as_string = isset( $nested_tags[0][ $index ] ) ? $nested_tags[0][ $index ] : '';
+				$attrs_string  = isset( $nested_tags[3][ $index ] ) ? $nested_tags[3][ $index ] : '';
+				$contents      = isset( $nested_tags[5][ $index ] ) ? $nested_tags[5][ $index ] : '';
 
-				$shortcode = new Dots_Compi_EBuilder_Shortcode( $this, $index, $as_string, $slug, $attrs_string, $contents );
+				$shortcode = new Dots_Compi_EBuilder_Shortcode( $this, $index, $old_as_string, $slug, $attrs_string, $contents );
 
-				$this->_children[] = $shortcode;
+				$this->add_child( $shortcode );
 
 				$index += 1;
 
@@ -699,50 +601,167 @@ class Dots_Compi_EBuilder_Shortcode extends Dots_Compi_EBuilder_Element {
 		}
 	}
 
-	private function maybe_add_column_wrapper() {
 
-		if ( true === $this->parent->is_row && ( false === $this->is_column || true === $this->is_resizable ) ) {
-			if ( 2 === $this->parent->get_len() && array_key_exists( $this->old_attrs, 'width' ) ) {
-				$width         = (int) $this->old_attrs['width'];
-				$sibling_index = ( 0 === $this->index ) ? 1 : 0;
-				$sibling       = $this->parent->get_child( $sibling_index );
-				$column_string = $sibling->column_wrapper;
-				if ( '1_4' === $column_string || $this->in_range( $width, 66, 100 ) ) {
-					$this->column_wrapper = '3_4';
-				} elseif ( '1_3' === $column_string || $this->in_range( $width, 56, 65 ) ) {
-					$this->column_wrapper = '2_3';
-				} elseif ( '1_2' === $column_string || $this->in_range( $width, 49, 55 ) ) {
-					$this->column_wrapper = '1_2';
-				} elseif ( '2_3' === $column_string || $this->in_range( $width, 26, 48 ) ) {
-					$this->column_wrapper = '1_3';
-				} elseif ( '3_4' === $column_string || $this->in_range( $width, 0, 25 ) ) {
-					$this->column_wrapper = '1_4';
-				}
+}
 
-			} elseif ( 2 === $this->parent->get_len() ) {
-				$this->column_wrapper = '1_2';
-			} elseif ( 1 === $this->parent->get_len() ) {
-				$this->column_wrapper = '4_4';
-			} elseif ( 3 === $this->parent->get_len() ) {
-				$this->column_wrapper = '1_3';
-			} elseif ( 4 === $this->parent->get_len() ) {
-				$this->column_wrapper = '1_4';
-			} else {
-				Dots_Compi_Conversion_Util::write_log( array( 'COLUMN WRAPPER ERROR' => 'TRUE', '$this->column_wrapper' => $this->column_wrapper ) );
-			}
-
-			if ( true === $this->is_resizable ) {
-				$this->_attrs['type'] = $this->column_wrapper;
-				$this->column_wrapper = false;
-			}
-		}
-	}
-
-
-	private function in_range( $val, $min, $max ) {
-
-		return ( $val >= $min && $val <= $max );
-	}
-
-
+class Dots_Compi_EBuilder_Mapping {
+	public static $mapping = array(
+		'et_lb_logo'          => array(
+			'new_slug' => 'et_pb_image',
+			'attrs'    => array(
+				'align'   => 'align',
+				'content' => 'url',
+			),
+		),
+		'et_lb_paper'         => array(
+			'new_slug' => 'et_pb_text',
+			'attrs'    => array(
+				'text'      => 'content_new',
+				'css_class' => 'module_class',
+			),
+		),
+		'et_lb_video'         => array(
+			'new_slug' => 'et_pb_text',
+			'attrs'    => array(
+				'video_url' => 'content_new',
+				'class'     => 'module_class',
+			),
+		),
+		'et_lb_testimonial'   => array(
+			'new_slug' => 'et_pb_testimonial',
+			'attrs'    => array(
+				'image_url'       => 'portrait_url',
+				'author_name'     => 'author',
+				'author_position' => 'job_title',
+				'author_site'     => 'url',
+				'content'         => 'content_new',
+			),
+		),
+		'et_lb_slogan'        => array(
+			'new_slug' => 'et_pb_text',
+			'attrs'    => array(
+				'class'   => 'module_class',
+				'content' => 'content_new',
+			),
+		),
+		'et_lb_slider'        => array(
+			'new_slug'  => 'et_pb_gallery',
+			'attrs'     => array(
+				'animation_duration' => 'auto_speed',
+				'auto_animation'     => 'auto',
+				'class'              => 'module_class',
+				'images'             => 'src',
+			),
+			'attrs_new' => array(
+				'fullwidth' => 'no',
+			),
+		),
+		'et_lb_button'        => array(
+			'new_slug' => 'et_lb_button',
+			'attrs'    => array(
+				'class' => 'module_class',
+			),
+		),
+		'et_lb_bar'           => array(
+			'new_slug' => 'et_pb_divider',
+			'attrs'    => array(
+				'class' => 'module_class',
+			),
+		),
+		'et_lb_list'          => array(
+			'new_slug' => 'et_pb_text',
+			'attrs'    => array(
+				'class'   => 'module_class',
+				'content' => 'content_new',
+			),
+		),
+		'et_lb_toggle'        => array(
+			'new_slug' => 'et_pb_toggle',
+			'attrs'    => array(
+				'heading' => 'title',
+				'state'   => 'open',
+				'class'   => 'module_class',
+				'content' => 'content_new',
+			),
+		),
+		'et_lb_simple_slider' => array(
+			'new_slug' => 'et_pb_slider',
+			'attrs'    => array(
+				'class' => 'module_class',
+			),
+		),
+		'et_lb_simple_slide'  => array(
+			'new_slug' => 'et_pb_slide',
+			'attrs'    => array(
+				'class' => 'module_class',
+			),
+		),
+		'et_lb_tabs'          => array(
+			'new_slug' => 'et_pb_tabs',
+		),
+		'et_lb_tab'           => array(
+			'new_slug' => 'et_pb_tab',
+			'attrs'    => array(),
+		),
+		'et_lb_pricing_table' => array(
+			'new_slug' => 'et_pb_pricing',
+			'attrs'    => array(),
+		),
+		'et_lb_box'           => array(
+			'new_slug' => 'et_pb_cta',
+			'attrs'    => array(),
+		),
+		'et_lb_text_block'    => array(
+			'new_slug' => 'et_pb_text',
+			'attrs'    => array(),
+		),
+		'et_lb_widget_area'   => array(
+			'new_slug' => 'et_pb_slider',
+			'attrs'    => array(),
+		),
+		'et_lb_image'         => array(
+			'new_slug' => 'et_pb_image',
+			'attrs'    => array(),
+		),
+		'et_lb_1_2'           => array(
+			'new_slug'  => 'et_pb_column',
+			'attrs'     => array(),
+			'attrs_new' => array(
+				'type' => '1_2',
+			),
+		),
+		'et_lb_1_3'           => array(
+			'new_slug'  => 'et_pb_column',
+			'attrs'     => array(),
+			'attrs_new' => array(
+				'type' => '1_3',
+			),
+		),
+		'et_lb_1_4'           => array(
+			'new_slug'  => 'et_pb_column',
+			'attrs'     => array(),
+			'attrs_new' => array(
+				'type' => '1_4',
+			),
+		),
+		'et_lb_2_3'           => array(
+			'new_slug'  => 'et_pb_column',
+			'attrs'     => array(),
+			'attrs_new' => array(
+				'type' => '2_3',
+			),
+		),
+		'et_lb_3_4'           => array(
+			'new_slug'  => 'et_pb_column',
+			'attrs'     => array(),
+			'attrs_new' => array(
+				'type' => '3_4',
+			),
+		),
+		'et_lb_resizable'     => array(
+			'new_slug'  => 'et_pb_column',
+			'attrs'     => array(),
+			'attrs_new' => array(),
+		),
+	);
 }
